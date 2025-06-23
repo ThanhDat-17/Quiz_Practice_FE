6952791353a13c762352658cbb5458909abe6ab9
hahoang1122 <155895286+hahoang1111@users.noreply.github.com>
2025-06-24 00:08:55 +0700

    done

---
+++ src/java/utils/EmailUtil.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import java.util.Properties;
+
+/**
+ *
+ * @author ADMIN
+ */
+// gửi mail
+public class EmailUtil {
+    private static final String EMAIL = "quizpracticefpt@gmail.com"; // Thay bằng email của bạn
+    private static final String PASSWORD = "xlxb ddng spvc jdab"; // Thay bằng app password (nếu dùng Gmail)
+
+    public static void sendEmail(String recipient, String subject, String content) throws MessagingException {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(EMAIL, PASSWORD);
+            }
+        });
+
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(EMAIL));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
+        message.setSubject(subject);
+        message.setContent(content, "text/html; charset=utf-8");
+
+        Transport.send(message);
+    }
+}
